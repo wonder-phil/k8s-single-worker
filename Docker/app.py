d87ba0bd9557b8ac197f48cff1c7ef4bcba88850
Phil Bradford <phillip.g.bradford@gmail.com>
2025-11-20 12:10:07 +0000

    init

---
+++ Docker/app.py
@@ -0,0 +1,32 @@
+from flask import request, Flask
+import json, socket
+import computeEngine 
+
+#import hashlib,random
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'This is backend  ! ' + str(socket.gethostname()) + ' \n'
+
+@app.route("/compute", methods=["POST"])
+def compute():
+    hostName = socket.gethostname()
+
+    bc = computeEngine.BackendCompute(hostName)
+    bc.processPRandomSeed()
+
+    limit = int(request.json['heads'])
+    print("limit: ", limit)
+
+    total_flips = bc.flipCoinsUntil(int(limit))
+    returnDictionary = {}
+    returnDictionary["hostName"] = hostName
+    returnDictionary["total_flips"] = total_flips
+    returnDictionary["heads"] = request.json['heads']
+
+    return json.dumps(returnDictionary)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
